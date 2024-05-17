56a17f03c04333b00be52c79d8831dd0c04162b4
TOXYGENCY <mailforsomethingelse@gmail.com>
2024-05-17 23:45:31 +0300

    Финал (чистка)

--- PI-Laba-5-Warehouse/Program.cs
+++ PI-Laba-5-Warehouse/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var wo = 10;
+            var w1 = new Warehouse(234);
+            var w2 = new Warehouse(10);
+            var w3 = new Warehouse(0);
+
+            w1.WriteOff(wo);
+            w2.WriteOff(wo);
+            w3.WriteOff(wo);
         }
     }
 }
---
+++ PI-Laba-5-Warehouse/Warehouse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI_Laba_5_Warehouse
+{
+    public class Warehouse
+    {
+        public int GoodsAmount { get; protected set; }
+
+        public Warehouse(int amount) 
+        {
+            if (amount < 0) throw new NegativeGoodsException();
+            GoodsAmount = amount;
+        }
+
+        public void WriteOff(int amount)
+        {
+            try
+            {
+                GoodsAmount -= amount;
+                if (GoodsAmount < 0) throw new NegativeGoodsException();
+            }
+            catch (NegativeGoodsException e) 
+            {
+                GoodsAmount = 0;
+                throw e; // по заданию надо кидать ошибку обратно
+            }
+            finally
+            {
+                Console.WriteLine($"Осталось товара на складе: {GoodsAmount}");
+            }
+
+        }
+    }
+}
