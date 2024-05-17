0b3391e574178585d03f34494fafe8359a257cd8
TOXYGENCY <mailforsomethingelse@gmail.com>
2024-05-17 23:07:02 +0300

    Add project files.

---
+++ PI-Laba-5-Warehouse/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PI_Laba_5_Warehouse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
